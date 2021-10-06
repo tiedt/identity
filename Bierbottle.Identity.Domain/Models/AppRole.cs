8b01ae8ec4ad6383ddbcd65e6c0a73775c97c75e
Guilherme Augusto Raduenz <graduenz@gmail.com>
2021-10-06 10:13:37 -0300

    added initial project structure

---
+++ Bierbottle.Identity.Domain/Models/AppRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bierbottle.Identity.Domain.Models
+{
+    public class AppRole
+    {
+        public Guid Id { get; set; }
+        public string UniqueName { get; set; }
+        public string FriendlyName { get; set; }
+    }
+}
